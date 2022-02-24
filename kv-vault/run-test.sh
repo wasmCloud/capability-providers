04f9e4a0751e88e40117f527e93e4a3c88256166
Steve <steve@cosmonic.com>
2022-02-23 23:20:06 -0800

    chagne LOCAL_IP to PORT
    Signed-off-by: Steve <steve@cosmonic.com>

--- kv-vault/run-test.sh
+++ kv-vault/run-test.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-
-# This script starts vault in dev mode in a docker container, and runs cargo tests
 set -e
 
-# name of host and port. Port should be unique to avoid conflicting with other open ports
-LOCAL_IP=127.0.0.1:11182
+# This script starts vault in dev mode in a docker container, and runs cargo tests
+
+# localhost port for server - should be unique to avoid conflicts
+PORT=11182
 # name of vault's temporary docker container
 CONTAINER_NAME=kv-vault-test
 # mount point, default is "secret"
@@ -25,13 +25,16 @@ cargo build ${RELEASE_FLAG} --all-features --all-targets
 cleanup
 
 # start vault docker in dev mode, no tls
+# scrape container log to get Root Token
 docker run --rm -d \
   --cap-add=IPC_LOCK \
   --name ${CONTAINER_NAME} \
-  -p ${LOCAL_IP}:8200 \
+  -p 127.0.0.1:${PORT}:8200 \
   vault:latest
 sleep 2
 export VAULT_TOKEN="$(docker logs ${CONTAINER_NAME} 2>&1 | grep 'Root Token:' | sed -E 's/Root Token: //')"
+
+# enable secrets engine
 # secret/ is mounted automatically so --path arg should only be used if VAULT_MOUNT is something else
 [[ -n "$VAULT_MOUNT" ]] && [[ "$VAULT_MOUNT" != "secret" ]] && [[ "$VAULT_MOUNT" != "secret/" ]] && PATH_ARG=-path=$VAULT_MOUNT
 docker exec -i -e VAULT_TOKEN=${VAULT_TOKEN} ${CONTAINER_NAME} \
@@ -41,7 +44,7 @@ docker exec -i -e VAULT_TOKEN=${VAULT_TOKEN} ${CONTAINER_NAME} \
 # run cargo test
 export RUST_BACKTRACE=1
 export RUST_LOG=${RUST_LOG}
-export VAULT_ADDR=http://${LOCAL_IP}
+export VAULT_ADDR=http://127.0.0.1:${PORT}
 [ -n "$VAULT_MOUNT" ] && export VAULT_MOUNT=${VAULT_MOUNT}
 cargo test ${RELEASE_FLAG} -- --nocapture
 
